cc50975b5be90bc73128217b16442e851c76b7d2
AngelicaMizutani <36971926+AngelMizutani@users.noreply.github.com>
2022-05-30 22:41:14 -0300

    Mais testes

--- testeCalendario/src/test/java/testeCalendario/TestaCalendario.java
+++ testeCalendario/src/test/java/testeCalendario/TestaCalendario.java
@@ -31,6 +31,7 @@ public class TestaCalendario {
 		System.setOut(new PrintStream(saidaOriginal));
 	}
 	
+	//V(1)
 	@Test
 	public void testarImpressaoDoAnoRecebidoPorParametro() throws IOException {
 		Path arquivo = Paths.get("src\\test\\resources", "2022.txt");
@@ -40,6 +41,7 @@ public class TestaCalendario {
 		assertEquals(ano2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
 	}
 	
+	//I(2)
 	@Test
 	public void testarQuantidadeDeParametrosMaiorQueDois() throws IOException {
 		Path arquivo = Paths.get("src\\test\\resources", "janeiro2022.txt");
@@ -48,10 +50,168 @@ public class TestaCalendario {
 		String obtido = saidaConsole.toString();
 		assertEquals(janeiro2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
 	}
+	
+	//I(3)
 	@Test
 	public void testarAnoNaoInteiro() {
 		Throwable excecao = assertThrows(NumberFormatException.class, 
 				() -> Calendario.mostrarCalendario("@"));
 		assertEquals("mostrarCalendario: @: ano inválido.", excecao.getMessage());
+	}
+	
+	//I(4)
+	@Test
+	public void testarAnoMenorQueUm() {
+		Throwable excecao = assertThrows(NumberFormatException.class, 
+				() -> Calendario.mostrarCalendario("0"));
+		assertEquals("mostrarCalendario: 0: ano inválido.", excecao.getMessage());
+	}
+	
+	//I(5)
+	@Test
+	public void testarAnoMaiorQue9999() {
+		Throwable excecao = assertThrows(NumberFormatException.class, 
+				() -> Calendario.mostrarCalendario("10000"));
+		assertEquals("mostrarCalendario: 10000: ano inválido.", excecao.getMessage());
+	}
+	
+	//I(6)
+	@Test
+	public void testarAnoEntre1E9999() throws IOException {
+		Path arquivo = Paths.get("src\\test\\resources", "2022.txt");
+		String ano2022 = Files.readString(arquivo); // saída esperada
+		Calendario.mostrarCalendario("2022");
+		String obtido = saidaConsole.toString();
+		assertEquals(ano2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
+	}
+	
+	//I(7)
+	@Test
+	public void testarMesAnoNaoInteiro() {
+		Throwable excecao = assertThrows(NumberFormatException.class, 
+				() -> Calendario.mostrarCalendario("@", "@"));
+		assertEquals("mostrarCalendario: @: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(8)
+	@Test
+	public void testarMesNaoInteiroAnoMenorQueUm() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("@", "0"));
+		assertEquals("mostrarCalendario: @: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(9)
+	@Test
+	public void testarMesNaoInteiroAnoMaiorQue9999() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("@", "10000"));
+		assertEquals("mostrarCalendario: @: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(10)
+	@Test
+	public void testarMesNaoInteiroAnoEntre1E9999() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("@", "2022"));
+		assertEquals("mostrarCalendario: @: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(11)
+	@Test
+	public void testarMesMenorQueUmAnoNaoInteiro() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("0", "@"));
+		assertEquals("mostrarCalendario: 0: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(12)
+	@Test
+	public void testarMesAnoMenorQueUm() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("0", "0"));
+		assertEquals("mostrarCalendario: 0: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(13)
+	@Test
+	public void testarMesMenorQueUmAnoMaiorQue9999() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("0", "10000"));
+		assertEquals("mostrarCalendario: 0: mês inválido.", excecao.getMessage());
+	}
+		
+	//I(14)
+	@Test
+	public void testarMesMenorQueUmAnoEntre1E9999() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("0", "2022"));
+		assertEquals("mostrarCalendario: 0: mês inválido.", excecao.getMessage());
+	}
+		
+	//I(15)
+	@Test
+	public void testarMesMaiorQue12AnoNaoInteiro() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("13", "@"));
+		assertEquals("mostrarCalendario: 13: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(16)
+	@Test
+	public void testarMesMaiorQue12AnoMenorQueUm() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("13", "0"));
+		assertEquals("mostrarCalendario: 13: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(17)
+	@Test
+	public void testarMesMaiorQue12AnoMaiorQue9999() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("13", "10000"));
+		assertEquals("mostrarCalendario: 13: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(18)
+	@Test
+	public void testarMesMaiorQue12AnoEntre1E9999() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("13", "2022"));
+		assertEquals("mostrarCalendario: 13: mês inválido.", excecao.getMessage());
+	}
+	
+	//I(19)
+	@Test
+	public void testarMesEntre1E12AnoNaoInteiro() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("5", "@"));
+		assertEquals("mostrarCalendario: @: ano inválido.", excecao.getMessage());
+	}
+	
+	//I(20)
+	@Test
+	public void testarMesEntre1E12AnoMenorQueUm() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("5", "0"));
+		assertEquals("mostrarCalendario: 0: ano inválido.", excecao.getMessage());
+	}
+	
+	//I(21)
+	@Test
+	public void testarMesEntre1E12AnoMaiorQue9999() {
+		Throwable excecao = assertThrows(NumberFormatException.class,
+				() -> Calendario.mostrarCalendario("5", "10000"));
+		assertEquals("mostrarCalendario: 10000: ano inválido.", excecao.getMessage());
+	}
+	
+	//V(22)
+	@Test
+	public void testarMesEntre1E12AnoEntre1E9999() throws IOException{
+		Path arquivo = Paths.get("src\\test\\resources", "janeiro2022.txt");
+		String janeiro2022 = Files.readString(arquivo); // saída esperada
+		Calendario.mostrarCalendario("1", "2022");
+		String obtido = saidaConsole.toString();
+		assertEquals(janeiro2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
 	}
 }
